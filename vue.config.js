35b78c1728e9f18fa3a17c7b6ae2bdbb2578c81b
tawfiqbhyry <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-08-17 19:05:27 +0200

    first_commit

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const Dotenv = require("dotenv-webpack");
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [new Dotenv()],
-  },
-});
+  transpileDependencies: true
+})
