4a88c1a10a20fd2e188c3af0dee1717f1334d4de
tawfiqbhyry <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-08-17 16:25:14 +0200

    first_commit

--- src/assets/utilities/firebase.js
+++ src/assets/utilities/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  databaseURL: process.env.DATABASE_URL,
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  databaseURL: `${process.env.DATABASE_URL}`,
 };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
