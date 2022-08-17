d4a82334c55092cd97871b55dce530579a0e40fc
tawfiqbhyry <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-08-17 19:40:35 +0200

    first_commit

---
+++ src/assets/utilities/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
+  appId: `${process.env.APP_ID}`,
+  databaseURL: `${process.env.DATABASE_URL}`,
+};
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const secondaryApp = initializeApp(firebaseConfig, "secondary");
+const auth = getAuth(firebaseApp);
+const db = getDatabase(firebaseApp);
+const secondDB = getDatabase(secondaryApp);
+const secondAuth = getAuth(secondaryApp);
+export { auth, db, secondDB, secondAuth };
