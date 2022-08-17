1f9e342fbfb0712803199090cb02b9775b62ed55
tawfiqbhyry <tawfik.2419010@stemdakahlia.moe.edu.eg>
2022-08-17 16:33:09 +0200

    first_commit

--- src/assets/utilities/firebase.js
+++ src/assets/utilities/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: `${process.env.STORAGE_BUCKET}`,
   messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
   appId: `${process.env.APP_ID}`,
-  databaseURL: `${process.env.DATABASE_URL}`,
+  databaseURL: `https://${process.env.DATABASE_URL}.firebaseio.com`,
 };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
