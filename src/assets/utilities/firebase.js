8c53d751083651189e3b8686e9a24884e0cb936b
Tawfiq Bhyry <81903419+tawfiqbhyry@users.noreply.github.com>
2022-08-18 17:12:19 +0200

    Update firebase.js

--- src/assets/utilities/firebase.js
+++ src/assets/utilities/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-console.log(process.env);
 const firebaseConfig = {
-  apiKey: `${process.env.API_KEY}`,
-  authDomain: `${process.env.AUTH_DOMAIN}`,
-  projectId: `${process.env.PROJECT_ID}`,
-  storageBucket: `${process.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-  appId: `${process.env.APP_ID}`,
-  databaseURL: `${process.env.DATABASE_URL}`,
+  apiKey: `${process.env.VUE_APP_API_KEY}`,
+  authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.VUE_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VUE_APP_MESSAGING_SENDER_ID}`,
+  appId: process.env.VUE_APP_APP_ID,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
 };
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const secondaryApp = initializeApp(firebaseConfig, "secondary");
