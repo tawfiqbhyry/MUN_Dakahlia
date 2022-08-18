84f50b685b05b6d5be057308d7ee03f91a3200e6
Tawfiq Bhyry <81903419+tawfiqbhyry@users.noreply.github.com>
2022-08-18 17:27:42 +0200

    Update firebase.js

--- src/assets/utilities/firebase.js
+++ src/assets/utilities/firebase.js
@@ -2,15 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
+
 const firebaseConfig = {
   apiKey: `${process.env.VUE_APP_API_KEY}`,
   authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
   projectId: `${process.env.VUE_APP_PROJECT_ID}`,
   storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
   messagingSenderId: `${process.env.VUE_APP_MESSAGING_SENDER_ID}`,
-  appId: process.env.VUE_APP_APP_ID,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  appId: `${process.env.VUE_APP_APP_ID}`,
+  databaseURL: `${process.env.VUE_APP_DATABASE_URL}`,
 };
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
