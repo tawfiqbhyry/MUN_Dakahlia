d017aa14d324f2909b84c06ab9c96b6e3c8ad179
Tawfiq Bhyry <81903419+tawfiqbhyry@users.noreply.github.com>
2023-02-05 20:56:29 +0200

    Update firebase.js

--- src/assets/utilities/firebase.js
+++ src/assets/utilities/firebase.js
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
-  apiKey: `${process.env.VUE_APP_API_KEY}`,
-  authDomain: `${process.env.VUE_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.VUE_APP_PROJECT_ID}`,
-  storageBucket: `${process.env.VUE_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.VUE_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.VUE_APP_APP_ID}`,
-  databaseURL: `${process.env.VUE_APP_DATABASE_URL}`,
+  apiKey: "AIzaSyA8sB7ygOsbkZZT7eUxzblQs1OIzvBTCDo",
+  authDomain: "mun-website-b80f5.firebaseapp.com",
+  databaseURL: "https://mun-website-b80f5-default-rtdb.firebaseio.com",
+  projectId: "mun-website-b80f5",
+  storageBucket: "mun-website-b80f5.appspot.com",
+  messagingSenderId: "693292642041",
+  appId: "1:693292642041:web:5019fc1c858b51673abc28",
+  measurementId: "G-MTG13PP7DK"
 };
 
 
@@ -21,4 +23,6 @@ const auth = getAuth(firebaseApp);
 const db = getDatabase(firebaseApp);
 const secondDB = getDatabase(secondaryApp);
 const secondAuth = getAuth(secondaryApp);
+const analytics = getAnalytics(firebaseApp);
+const analytics = getAnalytics(secondaryApp);
 export { auth, db, secondDB, secondAuth };
